9d5e2f5621bb1355290a70dbc9d283e8012a7b67
sherelynn <sherelynnb@missionreadyhq.com>
2025-04-15 11:16:10 +1200

    Change to available PORT number.

--- interview-app-backend/server.js
+++ interview-app-backend/server.js
@@ -14,7 +14,7 @@ if (!config.apiKey) {
 }
 
 //========== PORT ==========//
-const PORT = config.port || 5000
+const PORT = config.port || 5001
 
 app.listen(PORT, () => {
   console.log(`Server is running on: http://localhost:${PORT}`)
